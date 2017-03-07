22721fc364fc02f439d0db3a37b0afbfc53795c2
Trevor Bonomi <trevor@savvior.com>
2017-03-07 14:32:34 -0500

    junk update

--- src/js/savvicrop.js
+++ src/js/savvicrop.js
@@ -589,11 +589,11 @@ SavviCrop.prototype.createElements = function(el){
 	c += '<div class="sc-dropzone-msg">';
 	c += '<div class="head1"><span class="sc-image-area"></span><span class="sc-status"></span></div>';
 	c += '<div class="head2"><i class="fa fa-picture-o"></i> We recommend JPG, PNG or GIF</div>';
+	c += '</div>';
 	c += '<div class="head3">- or -</div>';
 	c += '<button class="btn"><i class="fa fa-upload"></i> Browse For Image</button>';
 	c += '<input type="file" class="sc-file-upload" name="ghost-file" title="Click to upload an image.">';
 	c += '<input type="text" style="width:1px;" class="sc-file-blob" required="'+self.options.required+'" id="'+self.options.id+'" name="'+self.options.id+'">';
-	c += '</div>';
 	c += '</div>';
 	c += '</div>';
 	c += '<div class="sc-toolbar clearfix">';
